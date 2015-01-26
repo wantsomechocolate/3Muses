4a73875996a6fcbbdb048240ff13b1893fbab3ac
wantsomechocolate <wantsomechocolate@gmail.com>
2015-01-26 08:51:52 -0500

    Sprucing up design

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b893654021a9c0de$465c8fbabc5b60fa66db5fac097acec81605dfa2"
+password="pbkdf2(1000,20,sha512)$b6ff70d29fcff9cd$7a3ac5809e4ab5f1a64204e3ccf8764fc9b94dde"
