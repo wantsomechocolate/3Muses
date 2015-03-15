9fda6f47d6bb6ac4816e7e98b774d90ede2bd828
wantsomechocolate <wantsomechocolate@gmail.com>
2015-03-15 07:37:13 -0400

    About to mess around with HTML5 Boilerplate

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9c72a68c656cd3ab$19c5946a58d68c484607d847d657ef56baee709f"
+password="pbkdf2(1000,20,sha512)$ab5fb1379c3eadc7$9e21059e6d4bfbbaef31584653dfbb5a7f7f8b69"
