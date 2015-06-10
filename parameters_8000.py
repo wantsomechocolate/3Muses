02b1f02e44d8f60a63e0b2e4cc0a3a83fc593d85
wantsomechocolate <wantsomechocolate@gmail.com>
2015-06-10 14:05:46 -0400

    fixed the non user auth features so the forms look better

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b0b1e307de0ff710$315f69bc70f73ec386460eebb5cceb6b8c09a10c"
+password="pbkdf2(1000,20,sha512)$927625445b9ee2f7$b5be0782598a804ded18feae72bbef0cc1cf961f"
